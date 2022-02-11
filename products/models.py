af6631743d4d73eab52f72cb79f2220de0d13e59
Desmond Onam <desmondonam@gmail.com>
2022-02-11 15:36:12 +0300

    show views

--- products/models.py
+++ products/models.py
@@ -1,3 +1,13 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
+
+
+class Product(models.Model):
+    name = models.CharField(_max_legth = 255)
+    price = models.FloatField()
+    stock = models.IntegerField()
+    image_url = models.CharField(max_length=2083)
+
+    
--- products/views.py
+++ products/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def index(request):
     return HttpResponse('Hello world')
+
+     
+def new(request):
+    return HttpResponse("New products")
